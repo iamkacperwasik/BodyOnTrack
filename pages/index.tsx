1405e75807923942f66f6219690571c746224c96
Kacper Wąsik <kacper@wasik.me>
2022-09-14 00:40:33 +0200

    add markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
-  return <h1 className='text-3xl font-bold'>KCalculator</h1>;
+  return (
+    <div className='p-5 flex gap-4 flex-col'>
+      <div>
+        <p>Age:</p>
+        <input type='number' className='border-[1px]' />
+      </div>
+      <div>
+        {/* TODO: switch  */}
+        <p>Weight (kg/lb)</p>
+        <input type='number' className='border-[1px]' />
+      </div>
+      <div>
+        {/* TODO: switch  */}
+        <p>Height (m/ft)</p>
+        <input type='number' className='border-[1px]' />
+      </div>
+      <div>
+        <p>Gender</p>
+        <div>
+          {/* TODO: switch  */}
+          <button>M</button>
+          <button>F</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
