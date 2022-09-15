6215497da28a4d115551cb4694ff106fad5746de
Kacper Wąsik <kacper@wasik.me>
2022-09-15 11:36:20 +0200

    add bmr formula

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from 'next';
 import {Else, If, Then, When} from 'react-if';
 import Form from '../components/Form';
 import getBMI from '../formulas/getBMI';
+import getBMR from '../formulas/getBMR';
 import {useBodyInfoStore} from '../stores/BodyInfo';
 
 const Home: NextPage = () => {
@@ -20,11 +21,8 @@ const Home: NextPage = () => {
         <Else>
           <When condition={age && weight && height}>
             <div className='mt-5'>
-              <p>Age: {age}</p>
-              <p>Weight: {weight}</p>
-              <p>Height: {height}</p>
-              <p>Gender: {gender === 'M' ? 'Male' : 'Female'}</p>
               <p>BMI: {getBMI(weight!, height!).toFixed(2)}</p>
+              <p>BMR: {getBMR(gender, weight!, height!, age!).toFixed(2)}</p>
             </div>
           </When>
         </Else>
