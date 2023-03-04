e40070d63a37768ffa0682e11ff8dc0ca6ddb4b8
Kacper Wąsik <kacper@wasik.me>
2023-03-04 01:55:30 +0100

    add noindex to head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import {Unless} from "react-if"
 
 import ActivityList from "components/Calculations/ActivityList"
@@ -22,29 +23,35 @@ const Home = () => {
   const formattedBMI = formatBMI(getBMI(weight!, height!))
 
   return (
-    <div className="mx-auto flex flex-col gap-8 p-4 md:w-1/2">
-      <Form />
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
 
-      <Unless condition={age === null || weight === null || height === null}>
-        <div className="flex flex-col gap-8">
-          <p className="text-2xl sm:text-3xl">Activity level</p>
-          <ActivityList />
+      <div className="mx-auto flex flex-col gap-8 p-4 md:w-1/2">
+        <Form />
 
-          <Goal />
-        </div>
+        <Unless condition={age === null || weight === null || height === null}>
+          <div className="flex flex-col gap-8">
+            <p className="text-2xl sm:text-3xl">Activity level</p>
+            <ActivityList />
 
-        <div className="mt-5 text-lg sm:text-2xl">
-          <p>
-            Your daily calorie intake: <b>{formattedCalories}</b>
-          </p>
-          <p>
-            Your bmi is: <b>{formattedBMI}</b>
-          </p>
-        </div>
+            <Goal />
+          </div>
 
-        <Chart />
-      </Unless>
-    </div>
+          <div className="mt-5 text-lg sm:text-2xl">
+            <p>
+              Your daily calorie intake: <b>{formattedCalories}</b>
+            </p>
+            <p>
+              Your bmi is: <b>{formattedBMI}</b>
+            </p>
+          </div>
+
+          <Chart />
+        </Unless>
+      </div>
+    </>
   )
 }
 
