1e7189b66003438e69c0b3e2e1a4a3f165c23a48
Kacper Wąsik <kacper@wasik.me>
2023-04-18 16:40:03 +0200

    use single hook for calories per day

--- hooks/useCaloriesPerDay.ts
+++ hooks/useCaloriesPerDay.ts
@@ -5,16 +5,22 @@ import {getBMR} from "formulas/BMR"
 
 import {activityLevelAtom} from "stores/Activity"
 import {ageAtom, heightAtom, sexAtom, weightAtom} from "stores/Body"
+import {calorieTargetAtom, goalAtom} from "stores/Goal"
 
-export const useCaloriesPerDay = () => {
+export const useCaloriesPerDay = (): number => {
   const age = useAtomValue(ageAtom)
   const weight = useAtomValue(weightAtom)
   const height = useAtomValue(heightAtom)
   const activityLevel = useAtomValue(activityLevelAtom)
   const sex = useAtomValue(sexAtom)
+  const goal = useAtomValue(goalAtom)
+  const calorieTarget = useAtomValue(calorieTargetAtom)
 
   const bmr = getBMR(sex, weight!, height!, age!)
-  const amr = getAMR(bmr, activityLevel)
+  const amr = getAMR(bmr, activityLevel) // calories per day
+
+  if (goal === "LOSE_WEIGHT") return amr - calorieTarget!
+  if (goal === "GAIN_WEIGHT") return amr + calorieTarget!
 
   return amr
 }
