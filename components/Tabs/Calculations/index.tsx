09afa866b31fc629795ac82242aa7f3a0442d768
Kacper Wąsik <kacper@wasik.me>
2023-04-20 01:05:16 +0200

    add input controlling forecast

--- components/Tabs/Calculations/index.tsx
+++ components/Tabs/Calculations/index.tsx
@@ -1,27 +1,39 @@
+import {useState} from "react"
+
 import {TableRow} from "components/Tabs/Calculations/TableRow"
+import {Input} from "components/UI/Input"
 
 import {useWeightForecast} from "hooks/useWeightForecast"
 
 export const Calculations = () => {
-  const forecast = useWeightForecast(100)
+  const [days, setDays] = useState<number | null>(null)
+
+  const forecast = useWeightForecast(days ?? 7)
 
   return (
-    <table className="">
-      <thead>
-        <tr className="text-2xl">
-          <th className="pr-8 text-left">Dzień</th>
-          <th className="px-8 text-left">Waga</th>
-          <th className="px-8 text-left">BMI</th>
-          <th className="px-8 text-left">Podaż kalorii</th>
-          <th className="px-8 text-left">CPM</th>
-        </tr>
-      </thead>
+    <>
+      <p className="text-2xl font-bold uppercase">
+        Na ile dni chcesz zobaczyć prognozę?
+      </p>
+      <Input state={[days, setDays]} placeholder="Ilość dni" />
 
-      <tbody>
-        {forecast.map((weight, day) => {
-          return <TableRow key={day} day={day} weight={weight} />
-        })}
-      </tbody>
-    </table>
+      <table className="">
+        <thead>
+          <tr className="text-2xl">
+            <th className="pr-8 text-left">Dzień</th>
+            <th className="px-8 text-left">Waga</th>
+            <th className="px-8 text-left">BMI</th>
+            <th className="px-8 text-left">Podaż kalorii</th>
+            <th className="px-8 text-left">CPM</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {forecast.map((weight, day) => {
+            return <TableRow key={day} day={day} weight={weight} />
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
