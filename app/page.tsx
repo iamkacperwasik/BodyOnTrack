ad8fc771477492341e22d00e79b3238430734ea3
Kacper Wąsik <kacper@wasik.me>
2023-09-11 23:28:19 +0200

    refactor: update layout + make it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,28 @@ import {EstimateButton} from "components/form/EstimateButton"
 
 export default function Home() {
   return (
-    <div className="container mx-auto mt-8 max-w-lg rounded-lg p-4 text-white shadow-md">
+    <div className="container mx-auto mt-8 max-w-2xl rounded-lg p-4 text-xl text-white shadow-md">
       <h1 className="mb-4 text-2xl font-semibold">Body Profile</h1>
 
-      <form>
-        <AgeInput />
-        <GenderSelect />
-        <HeightInput />
-        <WeightInput />
+      <form className="space-y-4">
+        <div className="-mx-2 flex flex-wrap justify-between">
+          <div className="w-full px-2 md:w-1/2">
+            <AgeInput />
+          </div>
+          <div className="w-full px-2 md:w-1/2">
+            <GenderSelect />
+          </div>
+        </div>
+
+        <div className="-mx-2 flex flex-wrap justify-between">
+          <div className="w-full px-2 md:w-1/2">
+            <HeightInput />
+          </div>
+          <div className="w-full px-2 md:w-1/2">
+            <WeightInput />
+          </div>
+        </div>
+
         <ActivityLevelSelect />
         <GoalSelect />
 
