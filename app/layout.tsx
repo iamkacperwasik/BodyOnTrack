e084c3bcd791d2d25cd7ac41f32af589607435bc
Kacper Wąsik <kacper@wasik.me>
2023-09-16 19:24:06 +0200

    feat: add matatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Link from "next/link"
 
 const inter = Inter({subsets: ["latin"]})
 
+export {metadata} from "metadata"
+
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
