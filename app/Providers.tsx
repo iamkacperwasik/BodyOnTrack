2bba63c0aab1e3e12b46fec77d3746fae488bf12
Kacper Wąsik <kacper@wasik.me>
2023-09-18 18:14:08 +0200

    feat: add nextui

---
+++ app/Providers.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import {NextUIProvider} from "@nextui-org/react"
+import {Provider as StateProvider} from "jotai"
+import {FC} from "react"
+
+type Props = {
+  children: React.ReactNode
+}
+
+export const Providers: FC<Props> = ({children}) => {
+  return (
+    <NextUIProvider>
+      <StateProvider>{children}</StateProvider>
+    </NextUIProvider>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import {Providers} from "Providers"
 import clsx from "clsx"
 import "globals.css"
 import {Inter} from "next/font/google"
@@ -10,40 +11,43 @@ export {metadata} from "metadata"
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={clsx(inter.className.toString(), "bg-black text-white")}>
-        {children}
+        <Providers>
+          {children}
 
-        <footer className="container mx-auto mt-8 max-w-2xl rounded-lg p-4 text-xl text-white shadow-md">
-          <div className="tsm:flex-col flex flex-col gap-2 text-sm sm:justify-between">
-            <p className="mb-2 text-lg sm:text-xl">
-              &quot;Empowering Your Health Journey, One Calorie at a Time&quot;
-            </p>
+          <footer className="container mx-auto mt-8 max-w-2xl rounded-lg p-4 text-xl text-white shadow-md">
+            <div className="tsm:flex-col flex flex-col gap-2 text-sm sm:justify-between">
+              <p className="mb-2 text-lg sm:text-xl">
+                &quot;Empowering Your Health Journey, One Calorie at a
+                Time&quot;
+              </p>
 
-            <div className="flex items-center gap-4 text-gray-300 ">
-              <div>
-                <span>&copy; 2023 </span>
+              <div className="flex items-center gap-4 text-gray-300 ">
+                <div>
+                  <span>&copy; 2023 </span>
+                  <Link
+                    href="https://kacper.wasik.me"
+                    target="_blank"
+                    className="hover:underline"
+                  >
+                    Kacper Wąsik
+                  </Link>
+                </div>
+
+                <div className="h-[15px] w-[2px] border-r border-gray-300" />
+
                 <Link
-                  href="https://kacper.wasik.me"
+                  href="https://github.com/iamkacperwasik/BodyOnTrack"
                   target="_blank"
                   className="hover:underline"
                 >
-                  Kacper Wąsik
+                  Source code
                 </Link>
               </div>
-
-              <div className="h-[15px] w-[2px] border-r border-gray-300" />
-
-              <Link
-                href="https://github.com/iamkacperwasik/BodyOnTrack"
-                target="_blank"
-                className="hover:underline"
-              >
-                Source code
-              </Link>
             </div>
-          </div>
-        </footer>
+          </footer>
+        </Providers>
 
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-6PYV8JEW10" />
         <Script id="google-analytics">
